c605bcb1c7bec658192e66e0059270b6307b0b3f
Christopher Hewett <chewett@hotmail.co.uk>
2016-06-30 23:57:14 +0100

    Added example command line running script

--- sync_db.py
+++ sync_db.py
@@ -1,3 +1,10 @@
 from WookieDb import WookieDb
 
-db = WookieDb.WookieDb()
+db = WookieDb.WookieDb(config_file="host_details.json")
+
+dbs = db.show_databases()
+for database in dbs:
+    db.select_db(database[0])
+    print db.show_tables()
+
+    print 'mysqldump.exe --host=localhost --protocol=tcp --user=root --lock-tables=FALSE --compress=TRUE --port=3306 --default-character-set=utf8 --skip-triggers "db" "table"'
