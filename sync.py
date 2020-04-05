04643df6fc9131d386619329d6703f215002f6e5
Christopher Hewett <chewett@hotmail.co.uk>
2020-04-05 17:25:42 +0100

    Make each segment optional

--- sync.py
+++ sync.py
@@ -9,19 +9,22 @@ backup_details = json.load(backup_detail_file)
 FTP_DUMP_LOC = "ftps"
 RSYNC_DUMP_LOC = "./rsync"
 
-for db_backup_name in backup_details['dbs']:
-    db_backup = DBBackupManager(db_backup_name, backup_details['dbs'][db_backup_name])
-    try:
+if "dbs" in backup_details:
+    for db_backup_name in backup_details['dbs']:
+        db_backup = DBBackupManager(db_backup_name, backup_details['dbs'][db_backup_name])
+        try:
+            db_backup.backup()
+        except Exception as e:
+            print "Failed to run backup on", db_backup_name
+            print e
+
+if "ftp" in backup_details:
+    for host_backup_name in backup_details['ftp']:
+        ftp_backup = FTPBackupManager(host_backup_name, backup_details['ftp'][host_backup_name])
+        ftp_backup.backup()
+
+if "rsync" in backup_details:
+    for rsync_backup_name in backup_details['rsync']:
+        db_backup = RsyncBackupManager(rsync_backup_name, backup_details['rsync'][rsync_backup_name])
         db_backup.backup()
-    except Exception as e:
-        print "Failed to run backup on", db_backup_name
-        print e
-
-for host_backup_name in backup_details['ftp']:
-    ftp_backup = FTPBackupManager(host_backup_name, backup_details['ftp'][host_backup_name])
-    ftp_backup.backup()
-
-for rsync_backup_name in backup_details['rsync']:
-    db_backup = RsyncBackupManager(rsync_backup_name, backup_details['rsync'][rsync_backup_name])
-    db_backup.backup()
 
